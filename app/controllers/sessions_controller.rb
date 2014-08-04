1ad27041115f219319def26ee7f607053af0c922
Noah Kaplan <noahkaplan4@gmail.com>
2014-08-04 15:40:37 -0700

    started user get and create methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   
   def create
+    env['ominiauth.auth']
     redirect_to landing_path
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth_hash)
+    where(auth_hash.slice("provider", "uid")).first || create_new_user(auth_hash)
+  end
+
+  def self.create_new_user(auth_hash)
+    raise auth_hash
+  end
+
 end
