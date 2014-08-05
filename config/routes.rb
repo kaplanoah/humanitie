bf7b1aa90fc488e47c3e086a638a478e02cac986
Noah Kaplan <noahkaplan4@gmail.com>
2014-08-05 11:25:12 -0700

    added selfie controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'selfie/index'
+
+  get 'selfie/new'
+
+  get 'selfie/create'
+
   root to: 'site#landing', as: 'landing'
 
   get 'auth/:provider/callback', to: 'sessions#create'
