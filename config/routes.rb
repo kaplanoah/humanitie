106e0813eb81958cff097366f14d48d4295d4cb0
Noah Kaplan <noahkaplan4@gmail.com>
2014-08-04 17:12:25 -0700

    added route to handle auth failures

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'site#landing', as: 'landing'
 
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
 
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
