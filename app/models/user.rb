6f3672845309231e509e1b2f951545039622f576
Noah Kaplan <noahkaplan4@gmail.com>
2014-09-09 22:12:02 -0700

    made selfies dependent on their user in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :selfies
+  has_many :selfies, dependent: :destroy
 
   def self.from_omniauth(auth_hash)
     where(auth_hash.slice("provider", "uid")).first || create_new_user(auth_hash)
