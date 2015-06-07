fa5c39a6bf8c5cbe227fbfbd6b70bc58f5c119d5
Noah Kaplan <noahkaplan4@gmail.com>
2015-06-07 01:24:04 -0700

    dont ask for or display image names, and default to timestamp

--- app/models/selfie.rb
+++ app/models/selfie.rb
@@ -1,10 +1,17 @@
 class Selfie < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   belongs_to :user
+  after_initialize :default_name
 
   def selfie_pair
     id_version = self.in_space ? 'space_selfie_id' : 'earth_selfie_id'
     return SelfiePair.find_by "#{id_version} = ?", self.id
+  end
+
+  private
+
+  def default_name
+    self.name ||= Time.now.to_i.to_s
   end
 
 end
